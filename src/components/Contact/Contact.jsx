318d2908748c13a92c9b8eb50564cd7af71ce051
alinakorobei88 <alinakorobei@gmail.com>
2024-07-18 00:13:27 +0200

    change3

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import styles from './Contact.module.css';
 const Contact = ({ id, name, number, deleteContact }) => {
   return (
     <li className={styles.contact}>
-      <p>{name}: {number}</p>
+      <p>{name} {number}</p>
       <button onClick={() => deleteContact(id)}>Delete</button>
     </li>
   );
